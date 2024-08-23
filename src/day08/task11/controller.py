46db9136c39bbb9d52854e8ba3203138d12d1380
itdanja <itdanja@kakao.com>
2024-08-23 15:06:53 +0900

    py day08 task11

--- src/day08/task11/controller.py
+++ src/day08/task11/controller.py
@@ -11,17 +11,17 @@ def samSungInfo() :
     for row in rows[1:] : # (4) 첫 줄 제외한 행 반복
         cols = row.split(',') #(5) 열 구분
         dic = { # 일자,종가,대비,등락률,시가,고가,저가,거래량,거래대금,시가총액,상장주식수
-            '일자' : cols[0] ,
-            '종가': cols[1],
-            '대비': cols[2],
-            '등락률': cols[3],
-            '시가': cols[4],
-            '고가': cols[5],
-            '저가': cols[6],
-            '거래량': cols[7],
-            '거래대금': cols[8],
-            '시가총액': cols[9],
-            '상장주식수': cols[9]}# (6) 딕셔너리 구성
+            '일자' : eval( cols[0] ) ,
+            '종가': format( int( eval( cols[1] ) ) , ',' ) , # 천단위 쉼표 : format( 숫자데이터 , ',' )
+            '대비': eval( cols[2] ) ,
+            '등락률': eval( cols[3] ),
+            '시가': eval( cols[4]),
+            '고가': eval( cols[5]),
+            '저가': eval( cols[6]),
+            '거래량': eval( cols[7]),
+            '거래대금': eval( cols[8]),
+            '시가총액': eval( cols[9]),
+            '상장주식수': eval( cols[9])}# (6) 딕셔너리 구성
         response.append( dic ) # (7) 리스트에 구성한 딕셔너리 담기
     return response # (8) 구성된 딕셔너리 리스트 반환
 
