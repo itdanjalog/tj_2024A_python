290f11c69da2bd8a0e6f037cb5d3edf0ae39e37a
itdanja <itdanja@kakao.com>
2024-08-23 12:36:34 +0900

    py day08 Flask

--- src/day08/task10/controller.py
+++ src/day08/task10/controller.py
@@ -7,15 +7,15 @@ def index() :
     data = personData() # 서비스 처리후 결과 받기
     # 파이썬 객체로는 JSON 전송이 불가능.
     # 파이썬 객체를 딕셔너리 로 변경 ,  # 객체명.__dict__
-    # 방법1]
+    # 방법1]  # 일반적인 반복문 이용한 새로운 리스트 생성
     # newData = [ ]
     # for o in data :
     #    dic = o.__dict__    # 객체를 딕셔너리로 변환
     #    newData.append( dic )
     # return newData
 
-    # 방법2]
-    # 위에 코드 4줄 -> 1줄 : map
+    # 방법2]  # 위에 코드 4줄 -> 1줄 : map
+    data = list( map( lambda o : o.__dict__ , data ) )
 
-    return
+    return data
 
