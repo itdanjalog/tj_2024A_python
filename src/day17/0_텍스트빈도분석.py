8d5a8c8841dfd122931eefa4df4618f4236b3489
MSI <kgs2072@naver.com>
2024-09-05 01:49:40 +0900

    -

--- src/day17/0_텍스트빈도분석.py
+++ src/day17/0_텍스트빈도분석.py
@@ -8,9 +8,11 @@ Big data can be analyzed for insights that lead to better decisions and strategi
 """
 
 import re
-ㅈ
+
 # 텍스트를 소문자로 변환
 text_data = text_data.lower()
+# text_data에 저장된 텍스트를 모두 소문자로 변환합니다. 이 단계는 동일한 단어가 대소문자로 인해 중복 계산되는 것을 방지합니다.
+
 
 # 구두점과 불필요한 기호 제거
 text_data = re.sub(r'[^\w\s]', '', text_data)
@@ -21,6 +23,7 @@ from collections import Counter
 
 # 텍스트를 단어 단위로 분할
 words = text_data.split()
+print( words )
 
 # 단어 빈도 계산
 word_freq = Counter(words)
@@ -29,3 +32,28 @@ word_freq = Counter(words)
 print(word_freq.most_common(10))
 ########################################################################
 # https://youngwoos.github.io/Doit_textmining/01-wordFrequency.html#4
+
+
+'''
+
+구두점 (Punctuation Marks):
+    마침표 (.): 문장의 끝을 나타냅니다.
+    쉼표 (,): 문장에서 일부분을 구분하거나 나열할 때 사용됩니다.
+    물음표 (?): 질문을 나타내는 데 사용됩니다.
+    느낌표 (!): 감정이나 강한 표현을 나타낼 때 사용됩니다.
+    콜론 (:): 설명을 시작하거나 목록을 열기 위해 사용됩니다.
+    세미콜론 (;): 서로 밀접하게 관련된 문장이나 문장 부분을 구분할 때 사용됩니다.
+    작은따옴표 ('): 인용구를 표시하거나 소유격을 나타낼 때 사용됩니다.
+    큰따옴표 ("): 직접 인용문을 나타낼 때 사용됩니다.
+    괄호 (()): 추가적인 설명이나 정보를 제공할 때 사용됩니다.
+특수 기호 (Special Characters):
+    @: 이메일 주소나 소셜 미디어에서 사용자 이름을 언급할 때 사용됩니다.
+    #: 해시태그로 사용되거나 번호를 나타낼 때 사용됩니다.
+    $: 화폐 단위를 나타낼 때 사용됩니다.
+    %: 백분율을 나타낼 때 사용됩니다.
+    &: 그리고를 의미하며, 보통 연결할 때 사용됩니다.
+    *: 별표 기호로, 강조하거나 중요성을 나타낼 때 사용됩니다.
+    - (하이픈): 단어를 연결하거나 구분할 때 사용됩니다.
+    _ (언더스코어): 공백 대신 사용되거나 이름을 구분할 때 사용됩니다.
+    
+'''
--- src/day17/1_영어단어분석.py
+++ src/day17/1_영어단어분석.py
@@ -18,6 +18,7 @@ from collections import Counter
 
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
+# pd.read_excel(file) # xlrd
 
 ### 1-1. 파일 병합
 all_files = glob.glob('myCabinetExcelData*.xls')
@@ -27,28 +28,29 @@ print( all_files )  #출력하여 내용 확인
 all_files_data = [] #저장할 리스트
 
 for file in all_files:
-    data_frame = pd.read_excel(file) # xlrd
+    data_frame = pd.read_excel(file)
     all_files_data.append(data_frame)
 
-print(  all_files_data[0])      #작업 내용 확인용 출력
+# print(  all_files_data[0])      #작업 내용 확인용 출력
 
 all_files_data_concat = pd.concat(all_files_data, axis=0, ignore_index=True)
 
-print(  all_files_data_concat)    #작업 내용 확인용 출력
+#print(  all_files_data_concat)    #작업 내용 확인용 출력
 
 all_files_data_concat.to_csv('riss_bigdata.csv', encoding='utf-8', index = False)
 
-### 1-2. 데이터 전처리 (Pre-processing)
-# 제목 추출
+# ### 1-2. 데이터 전처리 (Pre-processing)
+# # 제목 추출
 all_title = all_files_data_concat['제목']
-
-print( all_title ) #작업 내용 확인용 출력
-
+#
+# print( all_title ) #작업 내용 확인용 출력
+#
 stopWords = set(stopwords.words("english"))
 lemma = WordNetLemmatizer()
-
+#
 words = []
-
+#
+# ????????????????????????????????????????????????
 for title in all_title:
     EnWords = re.sub(r"[^a-zA-Z]+", " ", str(title))
     EnWordsToken = word_tokenize(EnWords.lower())
@@ -56,87 +58,89 @@ for title in all_title:
     EnWordsTokenStopLemma = [lemma.lemmatize(w) for w in EnWordsTokenStop]
     words.append(EnWordsTokenStopLemma)
 
-print(words)  #작업 내용 확인용 출력
-
+# print(words)  #작업 내용 확인용 출력
+# ?????????????????????????????????????????????????
 words2 = list(reduce(lambda x, y: x+y,words))
-print(words2)  #작업 내용 확인용 출력
-##################################################################################################################
-# 2. 데이터 탐색 및 분석 모델 구축
-## 2-1. 단어 빈도 탐색
+# print(words2)  #작업 내용 확인용 출력
+
+
+# ##################################################################################################################
+# # 2. 데이터 탐색 및 분석 모델 구축
+# ## 2-1. 단어 빈도 탐색0
 count = Counter(words2)
-print( count )   #작업 내용 확인용 출력
-
-word_count = dict()
-
-for tag, counts in count.most_common(50):
-    if(len(str(tag))>1):
-        word_count[tag] = counts
-        print("%s : %d" % (tag, counts))
-
-## 2-2 단어 빈도 히스토그램
-# 히스토그램 표시 옵션
-plt.figure(figsize=(12,5))
-plt.xlabel("word")
-plt.ylabel("count")
-plt.grid(True)
-
-sorted_Keys = sorted(word_count, key=word_count.get, reverse=True)
-sorted_Values = sorted(word_count.values(), reverse=True)
-
-plt.bar(range(len(word_count)), sorted_Values, align='center')
-plt.xticks(range(len(word_count)), list(sorted_Keys), rotation=85)
-
-plt.show()
-
-##################################################################################################################
-# 3. 결과 시각화
-## 3-1. 연도별 데이터 수
-all_files_data_concat['doc_count'] = 0
-summary_year = all_files_data_concat.groupby('출판일', as_index=False)['doc_count'].count()
-summary_year  #출력하여 내용 확인
-
-plt.figure(figsize=(12,5))
-plt.xlabel("year")
-plt.ylabel("doc-count")
-plt.grid(True)
-
-plt.plot(range(len(summary_year)), summary_year['doc_count'])
-plt.xticks(range(len(summary_year)), [text for text in summary_year['출판일']])
-
-plt.show()
-
-## 3-2. 워드클라우드
-stopwords=set(STOPWORDS)
-wc=WordCloud(background_color='ivory', stopwords=stopwords, width=800, height=600)
-cloud=wc.generate_from_frequencies(word_count)
-
-plt.figure(figsize=(8,8))
-plt.imshow(cloud)
-plt.axis('off')
-plt.show()
-
-#### - 워드 클라우드에 나타나는 단어의 위치는 실행 할 때마다 달라진다. ☺
-cloud.to_file("riss_bigdata_wordCloud.jpg")
-
-## 여기서 잠깐!! :
-### 검색어로 사용한 big'과 'data' 빈도가 압도적으로 많으므로, 정확한 단어 빈도 분석을 위해 검색어를 제거하고 단어 빈도 히스토그램을 다시 그려보자.
-#검색어로 사용한 'big'과 'data' 항목 제거 하기
-del word_count['big']
-del word_count['data']
-
-# 히스토그램 표시 옵션
-plt.figure(figsize=(12,5))
-plt.xlabel("word")
-plt.ylabel("count")
-plt.grid(True)
-
-sorted_Keys = sorted(word_count, key=word_count.get, reverse=True)
-sorted_Values = sorted(word_count.values(), reverse=True)
-
-plt.bar(range(len(word_count)), sorted_Values, align='center')
-plt.xticks(range(len(word_count)), list(sorted_Keys), rotation=85)
-
-plt.show()
+#print( count )   #작업 내용 확인용 출력
+#
+# word_count = dict()
+#
+# for tag, counts in count.most_common(50):
+#     if(len(str(tag))>1):
+#         word_count[tag] = counts
+#         print("%s : %d" % (tag, counts))
+#
+# ## 2-2 단어 빈도 히스토그램
+# # 히스토그램 표시 옵션
+# plt.figure(figsize=(12,5))
+# plt.xlabel("word")
+# plt.ylabel("count")
+# plt.grid(True)
+#
+# sorted_Keys = sorted(word_count, key=word_count.get, reverse=True)
+# sorted_Values = sorted(word_count.values(), reverse=True)
+#
+# plt.bar(range(len(word_count)), sorted_Values, align='center')
+# plt.xticks(range(len(word_count)), list(sorted_Keys), rotation=85)
+#
+# plt.show()
+#
+# ##################################################################################################################
+# # 3. 결과 시각화
+# ## 3-1. 연도별 데이터 수
+# all_files_data_concat['doc_count'] = 0
+# summary_year = all_files_data_concat.groupby('출판일', as_index=False)['doc_count'].count()
+# summary_year  #출력하여 내용 확인
+#
+# plt.figure(figsize=(12,5))
+# plt.xlabel("year")
+# plt.ylabel("doc-count")
+# plt.grid(True)
+#
+# plt.plot(range(len(summary_year)), summary_year['doc_count'])
+# plt.xticks(range(len(summary_year)), [text for text in summary_year['출판일']])
+#
+# plt.show()
+#
+# ## 3-2. 워드클라우드
+# stopwords=set(STOPWORDS)
+# wc=WordCloud(background_color='ivory', stopwords=stopwords, width=800, height=600)
+# cloud=wc.generate_from_frequencies(word_count)
+#
+# plt.figure(figsize=(8,8))
+# plt.imshow(cloud)
+# plt.axis('off')
+# plt.show()
+#
+# #### - 워드 클라우드에 나타나는 단어의 위치는 실행 할 때마다 달라진다. ☺
+# cloud.to_file("riss_bigdata_wordCloud.jpg")
+#
+# ## 여기서 잠깐!! :
+# ### 검색어로 사용한 big'과 'data' 빈도가 압도적으로 많으므로, 정확한 단어 빈도 분석을 위해 검색어를 제거하고 단어 빈도 히스토그램을 다시 그려보자.
+# #검색어로 사용한 'big'과 'data' 항목 제거 하기
+# del word_count['big']
+# del word_count['data']
+#
+# # 히스토그램 표시 옵션
+# plt.figure(figsize=(12,5))
+# plt.xlabel("word")
+# plt.ylabel("count")
+# plt.grid(True)
+#
+# sorted_Keys = sorted(word_count, key=word_count.get, reverse=True)
+# sorted_Values = sorted(word_count.values(), reverse=True)
+#
+# plt.bar(range(len(word_count)), sorted_Values, align='center')
+# plt.xticks(range(len(word_count)), list(sorted_Keys), rotation=85)
+#
+# plt.show()
 
 
 
