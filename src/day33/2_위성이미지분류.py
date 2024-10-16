1d20c62cddfd1cecb0a1343d0b283216240c1a62
itdanja <itdanja@kakao.com>
2024-10-16 13:20:18 +0900

    20241016 day33

--- src/day33/2_위성이미지분류.py
+++ src/day33/2_위성이미지분류.py
@@ -56,33 +56,72 @@ valid_data = valid_data.batch( BATCH_SIZE ).cache().prefetch( tf.data.AUTOTUNE )
 
 # prefetch( tf.data.AUTOTUNE ) : 데이터 전처리 와 훈련을 병렬로 수행하여 학습 속도를 향상 할수 있다.
 
-#
+# 모델링 함수
 def build_model():
-    model = tf.keras.Sequential([
-
+    model = tf.keras.Sequential([ # Sequential Api 이용한 모델 구축
+        # =========================== [ Convolution 층 = 합성곱 = 연산층 = 특징 찾기  ]
+        # 1. 합성곱(신경망) 레이어
         tf.keras.layers.BatchNormalization(),
+        # 배치 : 모델링에 있어서 병렬처리에 배치(묶음) 단위로 처리하면 더 빠르고 안정적으로 학습할수있다. # 과대적합을 줄이기 가능하다.
+        # BatchNormalization() 레이어가 없어도 모델 구현이 가능하지만 모델의 최적화에 필요한 레이어 # 주로 AUTOTUNE 사용시 사용된다.
         tf.keras.layers.Conv2D(32, (3, 3), padding='same', activation='relu'),
         tf.keras.layers.MaxPooling2D((2, 2)),
-
+        # 1. 합성곱(신경망) 레이어  # 복잡한 신경망 구현 하기 위해 2번의 합성곱을 실행했다.
+        # 1번만 합성곱 연산으로 모델 구축 가능 하지만 더 많은 특징을 찾기 위해 2번의 레이어 만들었다.
         tf.keras.layers.BatchNormalization(),
         tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu'),
         tf.keras.layers.MaxPooling2D((2, 2)),
-
+        # =========================== [ Classifier 층 = 출력층 = 예측분류층 = 특징 학습  ]
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dropout(0.3),
+        tf.keras.layers.Dense(128, activation='relu'), # Dense : 완전 연결된 신경망 층 , 이전 node(뉴런) 받아서 패턴 학습 한다.
+        # 주로 노드 개수 는 32 64 128 사용한다.
+        tf.keras.layers.Dropout(0.3),                   # 드롭아웃[ p.89 ] : 30% 노드를 랜덤으로 제외하고 학습 하는 설정 레이어
         tf.keras.layers.Dense(64, activation='relu'),
         tf.keras.layers.Dropout(0.3),
-        tf.keras.layers.Dense(NUM_CLASSES, activation='softmax'),
+        tf.keras.layers.Dense(NUM_CLASSES, activation='softmax'), # 출력층
     ])
     return model
 model = build_model()
 # 모델 컴파일
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 # 모델 훈련
-history = model.fit(train_data,
-                    validation_data=valid_data,
-                    epochs=3)
+history = model.fit( train_data,  validation_data=valid_data,  epochs = 3 )
+
+# 손실함수 , 정확도 그래프 그리기 # 교재에서 제외된 부분
+import matplotlib.pyplot as plt
+def plot_loss_acc( history , epoch  ) :
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    fig , axes  = plt.subplots( 1 , 2 )
+    axes[0].plot( range(1 , epoch + 1 ) , loss )
+    axes[0].plot( range(1, epoch + 1) , val_loss)
+    axes[0].set_title('Loss')
+
+    axes[1].plot( range(1 , epoch + 1) , acc )
+    axes[1].plot( range(1 , epoch + 1) , val_acc )
+    axes[1].set_title('Accuracy')
+
+    plt.show()
+
+plot_loss_acc( history , 3 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
